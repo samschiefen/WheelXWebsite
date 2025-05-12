74cfa72fea91a43a0cd094c56ae8dc9620637107
samschiefen <samschiefen@gmail.com>
2025-05-12 23:53:30 +0000

    Build

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Footer from './components/Footer';
 import MainDashboard from './pages/MainDashboard';
 import UserDashboard from './pages/UserDashboard';
